d7823c045fbdaf3c52c6a1f87cbea723105c2088
AQuena <alvaro.quena1@gmail.com>
2021-05-18 22:32:29 -0400

    primer commit

---
+++ src/test/java/clientAPI/RequestDELETE.java
@@ -0,0 +1,9 @@
+package clientAPI;
+
+public class RequestDELETE extends RequestClient{
+    @Override
+    public ResponseInformation send(RequestInformation request) {
+
+        return null;
+    }
+}
